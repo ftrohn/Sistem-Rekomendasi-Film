4ca4d61d9ae19698a82c14f1eb9b48f5c88ca35d
ardhiashaluna-ctrl <ardhiashaluna@gmail.com>
2026-06-04 12:13:30 +0700

    menambahkan search.py

---
+++ search.py
@@ -0,0 +1,13 @@
+def linear_search_genre(movies, game):
+    result = []
+    for movie in movies:
+        if genre.lower() in moovie.genre.lower():
+            result.append(movie)
+    return result
+
+def linear_search_title(movies, keyword):
+    result = []
+    for movie in movies:
+        if keyword.lower() in movie.title.lower():
+            result.append
+    return result
